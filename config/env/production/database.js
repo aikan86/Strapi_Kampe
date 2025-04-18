6f381e0a444ab10005be1904c153d4fee157dd8f
aikan86 <32703516+aikan86@users.noreply.github.com>
2025-04-19 01:38:28 +0200

    Modifiche locali prima del pull

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,16 @@
-// ./config/env/production/database.js
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false)
-      },
-      schema: env('DATABASE_SCHEMA', 'public'),
-    },
-    pool: {
-      min: env.int('DATABASE_POOL_MIN', 2),
-      max: env.int('DATABASE_POOL_MAX', 10),
-    },
-    debug: false,
-  },
-});
+   module.exports = ({ env }) => ({
+     connection: {
+       client: 'postgres',
+       connection: {
+         host: env('DATABASE_HOST'),
+         port: env.int('DATABASE_PORT'),
+         database: env('DATABASE_NAME'),
+         user: env('DATABASE_USERNAME'),
+         password: env('DATABASE_PASSWORD'),
+         ssl: {
+           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false)
+         }
+       }
+     }
+   });
+   
