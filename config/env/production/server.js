43b9036cca60bc83f140e01426f895c9043f0947
aikan86 <32703516+aikan86@users.noreply.github.com>
2025-04-19 18:55:34 +0200

    Update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: '0.0.0.0',
-  port: process.env.PORT || 1337,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
+    keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'https://kampe-strapi.onrender.com')
 });
+
