df2534f5709e4f124cb0d86e8a867fe3f8785676
aikan86 <32703516+aikan86@users.noreply.github.com>
2025-04-19 22:14:04 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:1337', 'http://localhost:3000']
+      origin: ['http://localhost:1337', 'http://localhost:3000','https://kampe-website.vercel.app']
     }
   },
   'strapi::poweredBy',
