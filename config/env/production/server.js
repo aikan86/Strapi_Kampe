9f8f6abf3da6503e0715b87161fcb56e9b07a3bb
aikan86 <32703516+aikan86@users.noreply.github.com>
2025-04-19 18:38:54 +0200

    Update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,8 @@
-   module.exports = ({ env }) => ({
-     host: env('HOST', '0.0.0.0'),
-     port: env.int('PORT', 1337),
-     app: {
-       keys: env.array('APP_KEYS')
-     },
-     url: env('PUBLIC_URL')
-   });
-   
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337), // Usa la porta fornita da Render o fallback su 1337
+  app: {
+    keys: env.array('APP_KEYS')
+  },
+  url: env('PUBLIC_URL', 'https://kampe-strapi.onrender.com')
+});
